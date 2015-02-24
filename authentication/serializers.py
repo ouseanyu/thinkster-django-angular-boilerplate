821df0fba7704e91d4916f8076c9bc728f54b424
Xiaodi Yu <xiaodi.yu@ou.edu>
2015-02-23 20:51:06 -0600

    make the first serializer

---
+++ authentication/serializers.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import update_session_auth_hash
+
+from rest_framework import serializers
+
+from authentication.models import Account
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
+    confirm_password = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = Account
+        fields = ('id', 'email', 'username', 'created_at', 'updated_at',
+                  'first_name', 'last_name', 'tagline', 'password',
+                  'confirm_password',)
+        read_only_fields = ('created_at', 'updated_at',)
+
+        def create(self, validate_data):
+            return Account.objects.create(**validate_data)
+
+        def update(self, instance, validate_data):
+            instance.username = validate_data.get('username', instance.username)
+            instance.tagline = validate_data.get('tageline', instance.tagline)
+
+            instance.save()
+
+            password = validate_data.get('password', None)
+            cofirm_password = validate_data.get('confirm_password', None)
+
+            if password and cofirm_password and password == cofirm_password:
+                instance.set_password(password)
+                instance.save()
+
+            update_session_auth_hash(self.context.get('request'), instance)
+
+            return instance
+
